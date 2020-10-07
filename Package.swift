548a343d785f64500ecca80b88254d17a67dca39
jayahariv <jayahariv88@gmail.com>
2020-10-06 20:22:08 -0700

    * corrected the URL to ee xcframework

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-sync-gateway/2.8.0/couchbase-sync-gateway-enterprise_2.8.0_x86_64.zip",
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/2.8.0/couchbase-lite-swift_xc_enterprise_2.8.0.zip",
             checksum: "5a9d9c8d0423f64ca7b3512f1da7ca572193f8c365aee2dc47cb47beb5632c41"
         )
     ]

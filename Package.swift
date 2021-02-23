706b83926a38687dc2ffddcf720d7d8277bf3a28
jayahariv <10448770+jayahariv@users.noreply.github.com>
2021-02-22 18:56:55 -0800

    GH-Action: Validate the swift package

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             name: "CouchbaseLiteSwift",
             url: "https://packages.couchbase.com/releases/couchbase-lite-ios/2.8.1/couchbase-lite-swift_xc_enterprise_2.8.1.zip",
             checksum: "e43568f03a67f796231144878e0bba5aa5c4d8ae0c0a39a795a2289fa1840530"
-        )
+        ),
+        .testTarget(
+            name: "CouchbaseLiteSwiftTests",
+            dependencies: ["CouchbaseLiteSwift"]),
     ]
 )

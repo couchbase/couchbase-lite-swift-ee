83edc4a36d105d5434b5df608a7e9fbe441fea95
Vlad Velicu <vlad_vel@yahoo.com>
2023-07-27 12:46:19 +0100

    CBL-4730: Remove testTarget and update actions validation (#17)
    * update actions
    
    * remove test target from package

--- Package.swift
+++ Package.swift
@@ -16,10 +16,6 @@ let package = Package(
             name: "CouchbaseLiteSwift",
             url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.0.12/couchbase-lite-swift_xc_enterprise_3.0.12.zip",
             checksum: "773aafaf819e9b00c1fa4d97de34bfb692921ccd99cae407a77292cef68f1c73"
-        ),
-        .testTarget(
-            name: "CouchbaseLiteSwiftTests",
-            dependencies: ["CouchbaseLiteSwift"]
         )
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import CouchbaseLiteSwiftTests
-
-var tests = [XCTestCaseEntry]()
-tests += CouchbaseLiteSwiftTests.allTests()
-XCTMain(tests)

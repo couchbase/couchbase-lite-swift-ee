69c982e51a1cc4b3c57f9604cf502a89ebfef450
Vlad Velicu <vlad.velicu@couchbase.com>
2024-01-12 18:18:55 +0000

    3.1.4 swift pack + readme

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.3/couchbase-lite-swift_xc_enterprise_3.1.3.zip",
-            checksum: "bed9175f2cd87cc379f67a765c6a061f44ec41210a520faab4e4a8280c9bae17"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.4/couchbase-lite-swift_xc_enterprise_3.1.4.zip",
+            checksum: "38cde970d32549515c2d3e91c6f87b85ed11c33d12cbd6ef27d2b0b231732eb9"
         )
     ]
 )

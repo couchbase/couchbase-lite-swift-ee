1a872e40413373997ce0496a5f164d39c7311383
Vlad Velicu <vlad_vel@yahoo.com>
2024-04-30 15:03:29 +0100

    Update SPM url and misc for beta 2 (#23)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.2.0-beta.1/couchbase-lite-swift_xc_enterprise_3.2.0-beta.1.zip",
-            checksum: "29ed7376a75036cc89b0857383fef58b609ef0de24f661b749323d38b03591a0"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.2.0-beta.2/couchbase-lite-swift_xc_enterprise_3.2.0-beta.2.zip",
+            checksum: "36b8e221e997e2721bc41833d78caf9cd16c780cc0a7ff78a655a901cde62d4f"
         )
     ]
 )

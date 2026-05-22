78a1fb353b9c5751a0e1da2464252bf7ef93b0bd
Pasin Suriyentrakorn <pasin@couchbase.com>
2026-05-22 09:43:58 -0700

    Update version to 4.0.4 (#40)
    * Updated version to 4.0.4
    * Disabled Github PR Validation as the validation script is not applicable for PR validatin. Will use Jenkins Job to validate instead.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/4.0.3/couchbase-lite-swift_xc_enterprise_4.0.3.zip",
-            checksum: "adddbf33d89e514918541b472eae34f776ca7ca65b5f692c78fafb8a56b36104"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/4.0.4/couchbase-lite-swift_xc_enterprise_4.0.4.zip",
+            checksum: "cbd0115b6afc5d15331dfa8783c5c70e7262a65fa277c11fedcec4ed0f634568"
         )
     ]
 )

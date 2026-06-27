03a58ff77ce1fbae71662cc44eb39c5431213ec5
Pasin Suriyentrakorn <pasin@couchbase.com>
2026-06-26 19:29:27 -0700

    Update version to 4.1.0
    * Updated binary and checksum to 4.1.0.
    
    * Updated version in README.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/4.0.4/couchbase-lite-swift_xc_enterprise_4.0.4.zip",
-            checksum: "cbd0115b6afc5d15331dfa8783c5c70e7262a65fa277c11fedcec4ed0f634568"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/4.1.0/couchbase-lite-swift_xc_enterprise_4.1.0.zip",
+            checksum: "55227d6ab1f73f69dc6d4c4bd40ed443a14a0d8255bad2ddd2dadd2ccbc9c2ac"
         )
     ]
 )

0d4d591dcc58329adbf4e2513f511824e3649ce0
Pasin Suriyentrakorn <pasin@couchbase.com>
2026-09-14 17:32:07 -0700

    Update version to 3.4.2

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.4.0/couchbase-lite-swift_xc_enterprise_3.4.0.zip",
-            checksum: "d6548852823ce4b887e42a6d81e1a072c227e2eca7f686d1d94cb7de438d6ea7"
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.4.2/couchbase-lite-swift_xc_enterprise_3.4.2.zip",
+            checksum: "d4a6a4e22ba2c4117b6c19c58b0c0ab158210a9d8baa990585f49822ba168edc"
         )
     ]
 )
